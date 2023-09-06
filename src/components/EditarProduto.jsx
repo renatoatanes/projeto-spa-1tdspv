3e3414fae1bc4b0ce45d96089703b04319a80bcf
renatoatanes <rm552029@fiap.com.br>
2023-09-05 22:38:49 -0300

    const id

--- src/components/EditarProduto.jsx
+++ src/components/EditarProduto.jsx
@@ -1,4 +1,13 @@
+import { ListaProdutos } from "./ListaProdutos";
+
 export default function EditarProdutos(){
+
+  
+    //Utilizando o hook paramns() para recuperar o id passado no path 
+
+    document.title = "EDITAR PRODUTOS" + Id;
+     
+    const ProdutoRetornaFltro = ListaProdutos.filter(produto => produto.id == id); 
 
     return(
         <main>
